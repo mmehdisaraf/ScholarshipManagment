2016b28e1f15234394d2e9f0e2d37db02e49f61e
mohammadmehdi <mohamadmehdi1999@gmail.com>
2019-12-15 23:36:38 +0330

    dashboard added

--- src/main/java/ir/mctab/java32/projects/scholarshipmanagement/ScholarshipManagementApplication.java
+++ src/main/java/ir/mctab/java32/projects/scholarshipmanagement/ScholarshipManagementApplication.java
@@ -16,7 +16,7 @@ public class ScholarshipManagementApplication {
 
         Scanner scanner = new Scanner(System.in);
         String command = "";
-        while (! command.equals("exit")) {
+        while (!command.equals("exit")) {
             System.out.println("what do you want? ");
             command = scanner.nextLine();
             // Login
@@ -31,108 +31,131 @@ public class ScholarshipManagementApplication {
                     System.out.println(" Login successful by " + user.getRole());
                 }
             }
-            if (command.equals("dashboard")){
-                DashboardStudentUseCase dashboardStudentUseCase =
-                        new DashboardStudentUseCaseImpl();
-                List<Scholarship> scholarship = dashboardStudentUseCase
-                        .listScholarships();
-                for (int i = 0; i < scholarship.size(); i++) {
-                    System.out.println(scholarship.get(i));}
-            }
-            //
-            if (command.equals("request")){
-                RequestScholarshipByStudentUseCase requestScholarshipByStudentUseCase
-                        = new RequestScholarshipByStudentUseCaseImpl();
-                Scanner scanner1 = new Scanner(System.in);
-                System.out.println("Please enter your name:");
-                String name = scanner1.nextLine();
-                System.out.println("Please enter your last_name:");
-                String family = scanner1.nextLine();
-                System.out.println("Please enter your nationalCode:");
-                String natinalCode = scanner1.nextLine();
-                System.out.println("Please enter your lastUni:");
-                String lastuni = scanner1.nextLine();
-                System.out.println("Please enter your lastDegree:");
-                String lastdegree = scanner1.nextLine();
-                System.out.println("Please enter your lastField:");
-                String lastfield = scanner1.nextLine();
-                System.out.println("Please enter your lastScore:");
-                Float score = scanner1.nextFloat();
-                String aaaaa = scanner1.nextLine();
-                System.out.println("Please enter your applyUni:");
-                String applyuni = scanner1.nextLine();
-                System.out.println("Please enter your applyDegree:");
-                String applydegree = scanner1.nextLine();
-                System.out.println("Please enter your applyField:");
-                String applyfield = scanner1.nextLine();
-                System.out.println("Please enter your applyDate:");
-                String applydate = scanner1.nextLine();
-                requestScholarshipByStudentUseCase.request(name,family,natinalCode,lastuni,lastdegree,lastfield,score,applyuni,applydegree,applyfield,applydate);
-
-            }
-            // find scholarship by supervisor
-            if (command.equals("svlist")) {
-                FindScholarshipBySupervisorUseCase findScholarshipBySupervisorUseCase
-                        = new FindScholarshipBySupervisorUseCaseImpl();
-
-                List<Scholarship> scholarships = findScholarshipBySupervisorUseCase
-                        .listScholarships();
-                for (int i = 0; i < scholarships.size(); i++) {
-                    System.out.println(scholarships.get(i));
+            if (command.equals("dashboard")) {
+                System.out.println("  1.Student Dashboard\n  2.Supervisor Dashboard\n  3.Manager Dashboard");
+                command = scanner.nextLine();
+                if (command.equals("Student Dashboard")) {
+                    DashboardStudentUseCase dashboardStudentUseCase =
+                            new DashboardStudentUseCaseImpl();
+                    List<Scholarship> scholarship = dashboardStudentUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarship.size(); i++) {
+                        System.out.println(scholarship.get(i));
+                    }
                 }
-            }
-            if (command.equals("svaccept")){
-                AcceptScholarshipBySupervisorUsecase acceptScholarshipBySupervisorUsecase =
-                        new AcceptScholarshipBySupervisorUseCaseImpl();
-                System.out.println("scholarship Id: ");
-                String scholarshipId = scanner.nextLine();
-                acceptScholarshipBySupervisorUsecase.accept(Long.parseLong(scholarshipId));
-                System.out.println("Done.");
-            }
-            if (command.equals("screject")){
-                RejectScholarshipBySupervisorUseCase rejectScholarshipBySupervisorUseCase =
-                        new RejectScholarshipBySupervisorUseCaseImpl();
-                System.out.println("scholarship Id: ");
-                String scholarshipId = scanner.nextLine();
-                rejectScholarshipBySupervisorUseCase.reject(Long.parseLong(scholarshipId));
-                System.out.println("Done.");
-            }
-            if (command.equals("mnlist")) {
-                FindScholarshipByManagerUseCase findScholarshipByManagerUseCase
-                        = new FindScholarshipByManagerUseCaseImpl();
-
-                List<Scholarship> scholarships = findScholarshipByManagerUseCase
-                        .listScholarships();
-                for (int i = 0; i < scholarships.size(); i++) {
-                    System.out.println(scholarships.get(i));
+                if (command.equals("Manager Dashboard")) {
+                    DashboardManagerUseCase dashboardManagerUseCase =
+                            new DashboardManagerUseCaseImpl();
+                    List<Scholarship> scholarship = dashboardManagerUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarship.size(); i++) {
+                        System.out.println(scholarship.get(i));
+                    }
                 }
-            }
-            if (command.equals("mnaccept")){
-                AcceptScholarshipByManagerUseCase acceptScholarshipByManagerUseCase =
-                        new AcceptScholarshipByManagerUseCaseImpl();
-                System.out.println("scholarship Id: ");
-                String scholarshipId = scanner.nextLine();
-                acceptScholarshipByManagerUseCase.accept(Long.parseLong(scholarshipId));
-                System.out.println("Done.");
-            }
-            if (command.equals("mnreject")){
-                RejectScholarshipByManagerUseCase rejectScholarshipByManagerUseCase =
-                        new RejectScholarshipByManagerUseCaseImpl();
-                System.out.println("scholarship Id: ");
-                String scholarshipId = scanner.nextLine();
-                rejectScholarshipByManagerUseCase.reject(Long.parseLong(scholarshipId));
-                System.out.println("Done.");
-            }
-            if (command.equals("unilist")){
-                FindScholarshipByUniversityUseCase findScholarshipByUniversityUseCase =
-                        new FindScholarshipByUniversityUseCaseImpl();
-                List<Scholarship> scholarships = findScholarshipByUniversityUseCase
-                        .listScholarships();
-                for (int i = 0; i < scholarships.size(); i++) {
-                    System.out.println(scholarships.get(i));
+                if (command.equals("Supervisor Dashboard")) {
+                    DashboardSupervisorUseCase dashboardSupervisorUseCase =
+                            new DashboardSupervisorUseCaseImpl();
+                    List<Scholarship> scholarship = dashboardSupervisorUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarship.size(); i++) {
+                        System.out.println(scholarship.get(i));
+                    }
+                }
+                //
+                if (command.equals("request")) {
+                    RequestScholarshipByStudentUseCase requestScholarshipByStudentUseCase
+                            = new RequestScholarshipByStudentUseCaseImpl();
+                    Scanner scanner1 = new Scanner(System.in);
+                    System.out.println("Please enter your name:");
+                    String name = scanner1.nextLine();
+                    System.out.println("Please enter your last_name:");
+                    String family = scanner1.nextLine();
+                    System.out.println("Please enter your nationalCode:");
+                    String natinalCode = scanner1.nextLine();
+                    System.out.println("Please enter your lastUni:");
+                    String lastuni = scanner1.nextLine();
+                    System.out.println("Please enter your lastDegree:");
+                    String lastdegree = scanner1.nextLine();
+                    System.out.println("Please enter your lastField:");
+                    String lastfield = scanner1.nextLine();
+                    System.out.println("Please enter your lastScore:");
+                    Float score = scanner1.nextFloat();
+                    String aaaaa = scanner1.nextLine();
+                    System.out.println("Please enter your applyUni:");
+                    String applyuni = scanner1.nextLine();
+                    System.out.println("Please enter your applyDegree:");
+                    String applydegree = scanner1.nextLine();
+                    System.out.println("Please enter your applyField:");
+                    String applyfield = scanner1.nextLine();
+                    System.out.println("Please enter your applyDate:");
+                    String applydate = scanner1.nextLine();
+                    requestScholarshipByStudentUseCase.request(name, family, natinalCode, lastuni, lastdegree, lastfield, score, applyuni, applydegree, applyfield, applydate);
 
+                }
+                // find scholarship by supervisor
+                if (command.equals("svlist")) {
+                    FindScholarshipBySupervisorUseCase findScholarshipBySupervisorUseCase
+                            = new FindScholarshipBySupervisorUseCaseImpl();
+
+                    List<Scholarship> scholarships = findScholarshipBySupervisorUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarships.size(); i++) {
+                        System.out.println(scholarships.get(i));
+                    }
+                }
+                if (command.equals("svaccept")) {
+                    AcceptScholarshipBySupervisorUsecase acceptScholarshipBySupervisorUsecase =
+                            new AcceptScholarshipBySupervisorUseCaseImpl();
+                    System.out.println("scholarship Id: ");
+                    String scholarshipId = scanner.nextLine();
+                    acceptScholarshipBySupervisorUsecase.accept(Long.parseLong(scholarshipId));
+                    System.out.println("Done.");
+                }
+                if (command.equals("screject")) {
+                    RejectScholarshipBySupervisorUseCase rejectScholarshipBySupervisorUseCase =
+                            new RejectScholarshipBySupervisorUseCaseImpl();
+                    System.out.println("scholarship Id: ");
+                    String scholarshipId = scanner.nextLine();
+                    rejectScholarshipBySupervisorUseCase.reject(Long.parseLong(scholarshipId));
+                    System.out.println("Done.");
+                }
+                if (command.equals("mnlist")) {
+                    FindScholarshipByManagerUseCase findScholarshipByManagerUseCase
+                            = new FindScholarshipByManagerUseCaseImpl();
+
+                    List<Scholarship> scholarships = findScholarshipByManagerUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarships.size(); i++) {
+                        System.out.println(scholarships.get(i));
+                    }
+                }
+                if (command.equals("mnaccept")) {
+                    AcceptScholarshipByManagerUseCase acceptScholarshipByManagerUseCase =
+                            new AcceptScholarshipByManagerUseCaseImpl();
+                    System.out.println("scholarship Id: ");
+                    String scholarshipId = scanner.nextLine();
+                    acceptScholarshipByManagerUseCase.accept(Long.parseLong(scholarshipId));
+                    System.out.println("Done.");
+                }
+                if (command.equals("mnreject")) {
+                    RejectScholarshipByManagerUseCase rejectScholarshipByManagerUseCase =
+                            new RejectScholarshipByManagerUseCaseImpl();
+                    System.out.println("scholarship Id: ");
+                    String scholarshipId = scanner.nextLine();
+                    rejectScholarshipByManagerUseCase.reject(Long.parseLong(scholarshipId));
+                    System.out.println("Done.");
+                }
+                if (command.equals("unilist")) {
+                    FindScholarshipByUniversityUseCase findScholarshipByUniversityUseCase =
+                            new FindScholarshipByUniversityUseCaseImpl();
+                    List<Scholarship> scholarships = findScholarshipByUniversityUseCase
+                            .listScholarships();
+                    for (int i = 0; i < scholarships.size(); i++) {
+                        System.out.println(scholarships.get(i));
+
+                    }
+                }
             }
         }
     }
-}
 }
